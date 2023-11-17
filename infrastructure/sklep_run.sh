5a881ba4a8ab7543a4e304e7218e141939b062f0
Madghostek <madghostek@gmail.com>
2023-11-17 02:30:33 +0100

    PIK-10 put .jar into docker image, use docker-compose to run everything, update setup scripts

--- infrastructure/sklep_run.sh
+++ infrastructure/sklep_run.sh
@@ -1,3 +1,13 @@
-docker run cassandra
 
-# tutaj będzie maven run
+sklep_internetowy/backend/sklep/mvnw compile
+
+# if docker commands don't work, then user needs docker group
+docker-compose up # on ubuntu it's still a separate program
+
+
+# how to run them by hand
+# docker run cassandra
+# docker run pis-sklep
+
+# connect to cassandra
+# docker run -it --network sklep_network --rm cassandra cqlsh some-cassandra
--- infrastructure/sklep_setup.sh
+++ infrastructure/sklep_setup.sh
@@ -1,13 +1,4 @@
-sudo apt install docker
+sudo apt install docker docker-buildx docker-compose
 
-# if docker commands don't work, then user needs docker group
-
-# create network (gives dns by container names)
-docker network create sklep_network
-
-
-docker create --name some-cassandra --network sklep_network -d cassandra
-
-
-# now this is possible
-# docker run -it --network sklep_network --rm cassandra cqlsh some-cassandra
+cd ~ && git clone ... # generate git credentials for vm
+cd sklep_internetowy
