84a1198af6048993798a01a0c38061b20c82ea8a
twezowic <01169258@pw.edu.pl>
2023-11-23 19:14:10 +0100

    PIK-12 Edit message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <span className="HelloText">Welcome</span>
+        <span className="HelloText">Hello</span>
       </header>
     </div>
   );
