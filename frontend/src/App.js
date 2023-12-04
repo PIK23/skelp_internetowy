b8eb25a1c4cbda33229e7a8fb25dcbab0317b43e
Madghostek <madghostek@gmail.com>
2023-12-05 00:57:51 +0100

    test push to master to see CD in action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <span className="HelloText">Hello</span>
+        <span className="CI/CD test">Hello</span>
       </header>
     </div>
   );
