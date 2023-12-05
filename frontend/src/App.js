8ed5106a18aa0a51bb46482c3b5fa79d711b4eca
mbogiel <95489139+mbogiel@users.noreply.github.com>
2023-12-05 23:30:20 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <span className="HelloText">Hello</span>
-        <span className="CI/CD test">Hello2</span>
+        <span className="CI/CD test">Witaj</span>
       </header>
     </div>
   );
