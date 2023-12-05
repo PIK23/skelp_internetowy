45f9b10c57d928e9c293a58950cc9a8924303f33
mbogiel <95489139+mbogiel@users.noreply.github.com>
2023-12-06 00:23:25 +0100

    Update SklepApplicationTests.java

--- backend/sklep/src/test/java/pl/projektpis/sklep/SklepApplicationTests.java
+++ backend/sklep/src/test/java/pl/projektpis/sklep/SklepApplicationTests.java
@@ -1,9 +1,9 @@
 package pl.projektpis.sklep;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.ActiveProfiles;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
