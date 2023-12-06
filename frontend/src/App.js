d2dea3d4ea62e3fbdb76edb71426a77f851b7c61
twezowic <01169258@pw.edu.pl>
2023-12-06 15:08:00 +0100

    Show products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,48 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.png';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8009/products');
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const jsonData = await response.json();
+        console.log('Pobrane dane:', jsonData);
+        setData(jsonData);
+      } catch (error) {
+        console.error('Błąd pobierania danych:', error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log('Render komponentu. Aktualne dane:', data);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <span className="HelloText">Hello</span>
+        {}
+        {data && (
+          <ul>
+            {data.map(item => (
+              <li key={item.nazwa}>{item.cena}</li>
+            ))}
+          </ul>
+        )}
         <span className="CI/CD test">Hello</span>
       </header>
     </div>
   );
-}
+};
 
 export default App;
