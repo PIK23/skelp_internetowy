09e6a6b6ac282ae819e8e1992f1009854f85b65d
twezowic <01169258@pw.edu.pl>
2023-12-24 01:03:01 +0100

    PIK-45 Finish simple routing and reorganise files.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,68 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Link, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProductDetail from './routes/ProductDetail';
+import ProductList from './routes/ProductList'
 import logo from './logo.png';
 import apple from './apple.jpg'
 import phone from './phone.jpg'
-import cart from './cart.png'
 import './App.css';
-
-const ProductItem = ({ product }) => {
-  const handleAddToCart = () => {
-    console.log(`Product added to cart: ${product.name}`);
-  };
-
-  return (
-    <tr>
-      <td><Link to={`/product/${product.name}`}>{product.name}</Link></td>
-      <td className="description">{product.description}</td>
-      <td>{product.price}</td>
-      <td>{product.weight}</td>
-      <td>{product.dimensions}</td>
-      <td><img alt={product.name} src={product.photo} height="64px" /></td>
-      <td>
-        <button onClick={handleAddToCart}>
-          <img type="submit" alt="Add to cart" src={cart} height="64px" />
-        </button>
-      </td>
-    </tr>
-  );
-};
-
-const Content = ({ products }) => {
-  return (
-    <Router>
-    <div className="Content">
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th>Weight</th>
-            <th>Dimensions</th>
-            <th>Photo</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product) => (
-            <ProductItem key={product.name} product={product} />
-          ))}
-        </tbody>
-      </table>
-      <Routes>
-          {products.map((product) => (
-            <Route
-              key={product.name}
-              path={`/product/${product.name}`}
-              element={<ProductDetail product={product} />}
-            />
-          ))}
-        </Routes>
-    </div>
-    </Router>
-  )
-};
 
 const App = () => {
   const [data, setData] = useState(null);
@@ -84,7 +27,6 @@ const App = () => {
       photo: phone,
     }
   ];
-
 
   // useEffect(() => {
   //   const fetchData = async () => {
@@ -111,14 +53,21 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Content products={sampleData} />
-
-        {/* dodawanie przez fetch
-        {data && <Content products={data} />}  */}
-
-        <span className="CI/CD test">Hello</span>
+        <Router>
+          <Routes>
+            <Route
+              path="/details/:id"
+              element={<ProductDetail products={sampleData} />}
+            />
+            <Route
+              path="/"
+              element={<ProductList products={sampleData} />}
+            />
+          </Routes>
+      </Router>
+      {/* <span className="CI/CD test">Hello</span> */}
       </header>
-    </div>
+  </div>
   )
 };
 
@@ -129,3 +78,6 @@ export default App;
 // poprawic fetch
 // dodac przezroczystość do koszyka
 // npm install react-router-dom
+
+    //     {/* dodawanie przez fetch
+    //     {data && <Content products={data} />}  */}
---
+++ frontend/src/routes/ProductList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import cart from './../cart.png'
+
+const ProductList = ({ products }) => {
+    const handleAddToCart = (productName) => {
+        console.log(`Product added to cart: ${productName}`);
+      };
+    return (
+      <div className='Margin'>
+        <table style={{ width: '100%' }}>
+            <tbody>
+            {products.map((product, index) => (
+                <tr key={index}>
+                <td style={{ display: 'flex', alignItems: 'center' }}>
+                  <img alt={product.name} src={product.photo} height="128px" style={{ marginRight: '10px' }} />
+                </td>
+                <td>
+                  <Link to={`/details/${index}`}>{product.name}</Link>
+                </td>
+                <td>{product.price}</td>
+                <td>
+                    <button onClick={() => handleAddToCart(product.name)}>
+                    <img alt="Add to cart" src={cart} height="64px" />
+                    </button>
+                </td>
+              </tr>
+            ))}
+            </tbody>
+      </table>
+      </div>
+    );
+  };
+
+export default ProductList;
