3fa72d557ab33c3006ece14488f2a700ba56b1d6
mbogiel <95489139+mbogiel@users.noreply.github.com>
2024-01-09 20:58:20 +0100

    vorect test to present app

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders hello link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello/i);
+  const linkElement = screen.getByText(/App/i);
   expect(linkElement).toBeInTheDocument();
 });
